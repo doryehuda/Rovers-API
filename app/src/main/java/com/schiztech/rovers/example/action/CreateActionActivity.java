75d193497e22c759a6ab7c6a6b87f13bea5fd119
Dor Yehuda <schiztech@gmail.com>
2015-05-22 17:20:10 +0300

    changed action's package name

--- app/src/main/java/com/schiztech/rovers/example/action/CreateActionActivity.java
+++ app/src/main/java/com/schiztech/rovers/example/action/CreateActionActivity.java
@@ -1,7 +1,9 @@
-package exampleaction.rovers.schiztech.com.roversactionexample;
+package com.schiztech.rovers.example.action;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import exampleaction.rovers.schiztech.com.roversactionexample.R;
 
 public class CreateActionActivity extends AppCompatActivity {
     @Override
