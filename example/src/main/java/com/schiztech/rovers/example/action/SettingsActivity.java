ea824ab28d7777c2bf160d85e823a2a78bed96b6
Dor Yehuda <schiztech@gmail.com>
2015-05-25 19:43:20 +0300

    added settings activity

---
+++ example/src/main/java/com/schiztech/rovers/example/action/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.schiztech.rovers.example.action;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
