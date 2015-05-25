a32e7d279934286f277e63f7780233399a6d37d3
Dor Yehuda <schiztech@gmail.com>
2015-05-25 19:32:25 +0300

    added api examples & example settings activity

--- example/src/main/java/com/schiztech/rovers/example/action/CreateActionActivity.java
+++ example/src/main/java/com/schiztech/rovers/example/action/CreateActionActivity.java
@@ -3,8 +3,6 @@ package com.schiztech.rovers.example.action;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.schiztech.rovers.api.RoversActionBuilder;
 
@@ -16,20 +14,28 @@ public class CreateActionActivity extends Activity {
         super.onCreate(savedInstanceState);
 
 
+        //pre-created an shortcut icon - just like on regular Android shortcuts
         Intent.ShortcutIconResource iconResource =
                 Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_action_roveraction);
 
 
+        //create the intent to be launched when the Rovers Action is triggered
         Intent launchIntent = new Intent(this, LaunchedActivity.class);
 
-        Intent result = RoversActionBuilder.build()
-                .setColor(getResources().getColor(R.color.material_deep_teal_200))
-                .setIconResource(iconResource)
-                .setLabel(getResources().getString(R.string.action_name))
-                .setIntent(launchIntent)
+        Intent roversActionIntent = RoversActionBuilder.build()
+                .setColor(getResources().getColor(R.color.material_deep_teal_200)) //sets the default color of the Rovers Action
+                .setIsColorChangeable(true)//determines if the user can change the default COLOR to another
+                .setIconResource(iconResource)//sets the default icon for the Rovers Action
+                //.setIcon(BITMAP object) //the default icon could also be a bitmap icon which will be cut to oval automatically by Rovers.
+                .setIsIconChangeable(true)//determines if the user can change the default ICON to another
+                .setLabel(getResources().getString(R.string.rovers_action_name))//the name of the Rovers Action, such as 'Call Home'
+                .setIntent(launchIntent)//the intent to be launched
                 .create();
 
-        setResult(RESULT_OK, result);
+
+        //just like a regular Android shortcut, the Rovers Action is created by a Result activity.
+        // The created action is transformed into an intent which will be sent to rovers as the result.
+        setResult(RESULT_OK, roversActionIntent);
 
         finish();
 
